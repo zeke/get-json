b7929c173e3b0a6dd52571b8fed77caed4cd2931
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-11-17 13:38:44 +0100

    Added a new test for the optional options parameter (https://github.com/zeke/get-json/pull/1#discussion_r84623788)

--- test.js
+++ test.js
@@ -9,3 +9,13 @@ test('get last libraries from cdnjs.com', function (t) {
     t.equal(body.keywords.length, 6);
   });
 });
+
+test('get repositories from GitHub', function (t) {
+  json('https://api.github.com/repositories', { 'headers': { 'User-Agent': 'A user agent' } }, function (error, body) {
+    t.plan(4);
+    t.error(error);
+    t.ok(body[0]);
+    t.ok(body[0].name);
+    t.equal(body.length, 100);
+  });
+});
